02918f94eee57c03de9ba8b7670f3989a133039b
Karm Parker <karmparker@gmail.com>
2025-12-26 00:01:35 -0500

    Add start:web script with environment variable and debug logging

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
 
+// Log to verify .env is loaded (only in development)
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Environment variables loaded:', {
+    hasMapboxToken: !!process.env.EXPO_PUBLIC_MAPBOX_TOKEN,
+    hasApiUrl: !!process.env.EXPO_PUBLIC_API_URL,
+    mapboxTokenPreview: process.env.EXPO_PUBLIC_MAPBOX_TOKEN ? 
+      process.env.EXPO_PUBLIC_MAPBOX_TOKEN.substring(0, 20) + '...' : 'not set'
+  });
+}
+
 module.exports = {
   expo: {
     name: "WeatherRoute",
@@ -27,11 +37,16 @@ module.exports = {
         "ACCESS_COARSE_LOCATION"
       ]
     },
-    web: {},
+    web: {
+      // Configure webpack to inject environment variables
+      config: {
+        plugins: []
+      }
+    },
     extra: {
       // Make environment variables available to the app
-      mapboxToken: process.env.EXPO_PUBLIC_MAPBOX_TOKEN,
-      apiUrl: process.env.EXPO_PUBLIC_API_URL
+      mapboxToken: process.env.EXPO_PUBLIC_MAPBOX_TOKEN || '',
+      apiUrl: process.env.EXPO_PUBLIC_API_URL || 'https://weathe1-production.up.railway.app/api'
     }
   }
 };
